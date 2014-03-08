cabf6f82c657839ea71087b48ef213574e04f7bd
Mark Chenpingling <mark@livehereandnow.com>
2014-03-08 23:37:06 +0800

    GTP baisc, done! working dummy Go Program to attach to GoGui

--- src/com/opensudoku/go/Go.java
+++ src/com/opensudoku/go/Go.java
@@ -6,11 +6,13 @@
 package com.opensudoku.go;
 
 import com.opensudoku.go.exception.GoBadException;
+import java.io.BufferedWriter;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 /**
  *
@@ -37,43 +39,36 @@ public class Go implements Coordinate {
     }
 
     public void sendGpt(String str) throws IOException {
-//    InputStreamReader cin = new InputStreamReader(System.in);
-        //  FileOutputStream fos= new FileOutputStream(System.out);
+        Writer osr
+                = new BufferedWriter(new OutputStreamWriter(System.out));
+        osr.write(str+"\n\n");
+        osr.flush();
 
-//        osr.write(str.toCharArray());
-//        System.out.println(str);
-//    OutputStream out = System.out;
-//    OutputStreamWriter osr = new OutputStreamWriter(out);
-        OutputStream out = System.out;
-        try (OutputStreamWriter osr = new OutputStreamWriter(out)) {
-            osr.write(str);
-            osr.flush();
-        }catch(Exception e){
-            System.out.println(e.toString());
-        }
     }
 
     public boolean gtpCommand(String cmd) throws IOException {
 
         switch (cmd.trim()) {
             case "name": {
-                sendGpt("= OpenSudokuGo \n\n");
+                sendGpt("= OpenSudokuGo ");
                 return true;
             }
             case "protocol_version": {
-                sendGpt("= 2 \n\n");
+                sendGpt("= 2 ");
                 return true;
             }
             case "version": {
-                sendGpt("= 1.0\n\n");
+                sendGpt("= 1.0 ");
                 return true;
             }
             case "list_commands": {
-                sendGpt("= name");
-                sendGpt("protocol_version");
-                sendGpt("version");
-                sendGpt("boardsize");
-                sendGpt("clear_board");
+                StringBuilder sb=new StringBuilder();
+                sb.append("= name\n");
+                sb.append("protocol_version\n");
+                sb.append("version\n");
+                sb.append("boardsize\n");
+                sb.append("clear_board\n");
+                sendGpt(sb.toString());
                 return true;
             }
             case "boardsize 19": {
@@ -88,8 +83,8 @@ public class Go implements Coordinate {
                 sendGpt("= \n");
                 return true;
             }
-            case "genemove b": {
-                sendGpt("= q16\n");
+            case "genmove b": {
+                sendGpt("= q16");
                 return true;
             }
 
